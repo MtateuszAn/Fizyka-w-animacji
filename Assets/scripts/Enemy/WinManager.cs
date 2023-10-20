91a8b1cd23a5d5bf86e0fa816bf1a0b0ab6f8995
Mateusz Niekłań <42940614+MtateuszAn@users.noreply.github.com>
2023-10-20 16:07:59 +0200

    Dodanie komentarza

--- Assets/scripts/Enemy/WinManager.cs
+++ Assets/scripts/Enemy/WinManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 public class WinManager : MonoBehaviour
 {
@@ -17,6 +16,7 @@ public class WinManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //wykonuje sie jezeli ilosc obiektow do zniszczenia jest rowna 0
         if(gameObject.transform.childCount == 0)
         {
             WinText.gameObject.SetActive(true);
